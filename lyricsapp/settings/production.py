a88d6341faccb815003eb7f27883f587d9841c5a
Dosia <dominika.gajda@gmail.com>
2020-05-28 12:15:46 +0200

    Initial Heroku commit

--- lyricsapp/settings/production.py
+++ lyricsapp/settings/production.py
@@ -1,16 +1,29 @@
+import os
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+}
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['lyrics-app-api.herokuapp.com']
 
-CORS_ORIGIN_ALLOW_ALL = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
 
-# SECRET_KEY = ""
-# SPOTIFY_CLIENT_ID = ""
-# SPOTIFY_CLIENT_SECRET = ""
-# YOUTUBE_API_KEY = ""
+CORS_ORIGIN_ALLOW_ALL = True
 
-# DB
-
-CORS_ORIGIN_WHITELIST = [
-
-]
+# CORS_ORIGIN_WHITELIST = [
+#
+# ]
